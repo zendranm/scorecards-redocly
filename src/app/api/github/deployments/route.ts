7cdfb5b810770029f8df8c31a8f3b12685e66598
Michał Zendran <zendran.m@gmail.com>
2026-02-12 10:39:40 +0100

    Refactoring

--- src/app/api/github/deployments/route.ts
+++ src/app/api/github/deployments/route.ts
@@ -1,18 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { fetchDeployments } from "@/lib/fetchDeployments";
 
-const owner = process.env.GITHUB_OWNER;
-const repo = process.env.GITHUB_REPO;
-if (!owner || !repo) {
-  throw new Error("GITHUB_OWNER and GITHUB_REPO must be set");
-}
-
 export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams;
-    const ownerParam = searchParams.get("owner") || owner;
-    const repoParam = searchParams.get("repo") || repo;
-
+    const ownerParam = searchParams.get("owner");
+    const repoParam = searchParams.get("repo");
     const deployments = await fetchDeployments(
       ownerParam as string,
       repoParam as string
